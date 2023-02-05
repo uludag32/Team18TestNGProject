89869ef848611c494a8199a86221afdde1372eb2
Omer Uludag <109936007+uludag32@users.noreply.github.com>
2023-02-05 15:44:13 +0300

    Fatih (#5)
    * US13
    
    * US13
    
    * US13
    
    ---------
    
    Co-authored-by: QAFatih <fatih.kupeli80@gmail.com>

--- src/test/java/tests/US13/US13TC01.java
+++ src/test/java/tests/US13/US13TC01.java
@@ -4,6 +4,8 @@ public class US13TC01 {
 
     public static void main(String[] args) {
         System.out.println("fatih");
+        System.out.println("fatih");
+        System.out.println("fatih");
     }
 
 }
