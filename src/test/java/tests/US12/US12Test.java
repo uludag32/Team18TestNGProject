a564480d1eb5f216ab832e4539db81ed13340f36
QAFatih <117031793+QAFatih@users.noreply.github.com>
2023-02-07 01:01:47 +0300

    US12 (#6)

--- src/test/java/tests/US12/US12Test.java
+++ src/test/java/tests/US12/US12Test.java
@@ -1,11 +1,12 @@
 package tests.US12;
 
+import org.openqa.selenium.WebElement;
 import org.testng.annotations.Test;
 import pages.fatih.SpendingGood_HomePage;
 import pages.fatih.SpendingGood_LoginPage;
 import utilities.ConfigReader;
 import utilities.Driver;
-
+import utilities.ReusableMethods;
 
 
 public class US12Test {
@@ -14,20 +15,20 @@ public class US12Test {
 
     @Test
 
-    public void us12(){
+    public void us12TC01(){
         Driver.getDriver().get(ConfigReader.getProperty("spending_Good_Url"));
 
        spendingGood_homePage=new SpendingGood_HomePage();
        spendingGood_loginPage=new SpendingGood_LoginPage();
        spendingGood_homePage.Sign_In.click();
 
-        spendingGood_homePage=new SpendingGood_HomePage();
-        spendingGood_loginPage=new SpendingGood_LoginPage();
-        spendingGood_homePage.Sign_In.click();
-
-        spendingGood_homePage.userName.sendKeys(ConfigReader.getProperty("vendor_username"));
-        spendingGood_loginPage.password.sendKeys(ConfigReader.getProperty("vendor_password"));
+        spendingGood_homePage.username.sendKeys(ConfigReader.getProperty("vendor_username"));
+        spendingGood_homePage.password.sendKeys(ConfigReader.getProperty("vendor_password"));
         spendingGood_homePage.SignIN.click();
+        ReusableMethods.verifyElementDisplayed(spendingGood_homePage.username);
+
+
+
 
 
 
