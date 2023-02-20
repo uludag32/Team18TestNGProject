234ea4db725b5a7bb6a936509ad32277abf4af50
QAFatih <fatih.kupeli80@gmail.com>
2023-02-20 20:28:28 +0300

    Fatih

--- src/test/java/pages/fatih/SpendingGood_LoginPage.java
+++ src/test/java/pages/fatih/SpendingGood_LoginPage.java
@@ -1,10 +1,11 @@
 package pages.fatih;
 
-import org.openqa.selenium.By;
+
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import utilities.Driver;
+
 
 public class SpendingGood_LoginPage {
     public SpendingGood_LoginPage() {
