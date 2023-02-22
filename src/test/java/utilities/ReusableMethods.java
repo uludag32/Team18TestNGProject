a25bd262c85a943e0ef0853a1c7bac8c8fac892e
ilkerozdemir01 <115585933+ilkerozdemir01@users.noreply.github.com>
2023-02-23 01:02:04 +0300

    Ilker (#24)
    * ilker
    
    * 15.02.2023-00:57
    
    * ilker
    
    * ilker
    
    * ilker
    
    * v2
    
    * deleted faker class

--- src/test/java/utilities/ReusableMethods.java
+++ src/test/java/utilities/ReusableMethods.java
@@ -18,9 +18,7 @@ import java.util.function.Function;
 
 import static org.testng.AssertJUnit.assertFalse;
 import static org.testng.AssertJUnit.assertTrue;
-
 public class ReusableMethods {
-
     public static String getScreenshot(String name) throws IOException {
         // naming the screenshot with the current date to avoid duplication
         String date = new SimpleDateFormat("yyyyMMddhhmmss").format(new Date());
